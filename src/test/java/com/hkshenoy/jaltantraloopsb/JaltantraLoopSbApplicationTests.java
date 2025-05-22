fd5cbd8358a58799a216f1e71e2c6fa84204403f
cse2001 <ankushmitra17@gmail.com>
2025-05-23 01:51:16 +0530

    test 7 added

--- src/test/java/com/hkshenoy/jaltantraloopsb/JaltantraLoopSbApplicationTests.java
+++ src/test/java/com/hkshenoy/jaltantraloopsb/JaltantraLoopSbApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
