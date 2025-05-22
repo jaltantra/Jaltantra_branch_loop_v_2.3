7443c37011f1ff75b8fadb42c70b07a38438373c
cse2001 <ankushmitra17@gmail.com>
2025-05-23 01:44:02 +0530

    test 6 added

--- src/test/java/com/hkshenoy/jaltantraloopsb/JaltantraLoopSbApplicationTests.java
+++ src/test/java/com/hkshenoy/jaltantraloopsb/JaltantraLoopSbApplicationTests.java
@@ -2,8 +2,14 @@ package com.hkshenoy.jaltantraloopsb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+
+
 class JaltantraLoopSbApplicationTests {
 
     @Test
