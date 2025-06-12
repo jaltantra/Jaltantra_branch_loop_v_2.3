68c456473b638879da40e8aba04bfbec117df482
uv287 <utsavmananiuv@gmail.com>
2025-06-12 14:05:40 +0530

    Run script update

--- run.sh
+++ run.sh
@@ -5,10 +5,4 @@
 ./mvnw package
 
 # Run Spring Boot app in background
-java -jar target/JaltantraLoopSB-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev > /dev/null 2>&1 &
-
-# Run Python script in background
-python3 /path/to/your_python_script.py > /dev/null 2>&1 &
-
-# Optional: Wait or log to confirm
-echo "Spring Boot and Python applications started in background."
+java -jar target/JaltantraLoopSB-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev > /dev/null 2>&1
