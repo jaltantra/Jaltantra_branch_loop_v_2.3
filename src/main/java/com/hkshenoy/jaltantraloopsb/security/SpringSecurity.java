42a5ffc38e621238611458ab7845e93f4e6510d7
cse2001 <ankushmitra17@gmail.com>
2025-06-13 00:56:12 +0530

    2 files changed

--- src/main/java/com/hkshenoy/jaltantraloopsb/security/SpringSecurity.java
+++ src/main/java/com/hkshenoy/jaltantraloopsb/security/SpringSecurity.java
@@ -43,7 +43,7 @@ public class SpringSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/","/images/**","/css/**","/register/**","/verify/**").permitAll()
+                        authorize.requestMatchers("/","/images/**","/css/**","/register/**","/verify/**","/actuator/prometheus/**").permitAll()
                         //authorize.requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
 
